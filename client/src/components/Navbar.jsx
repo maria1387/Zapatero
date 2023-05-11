050a01b0239ec17e31803259848fca411c9f7eda
Pilar Abarca <mariapilar.abarca@gmail.com>
2023-05-11 16:43:21 -0400

    prueba

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,14 +43,9 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-<<<<<<< HEAD
     protectedInfo()
  
   }, [])
-=======
-    protectedInfo();
-  }, []);
->>>>>>> db0443a396991cf5b2fd4e765b866fc5d8077bca
 
   return (
 
@@ -68,35 +63,10 @@ const Navbar = () => {
               Zapatero
             </h1>
           </div>
-<<<<<<< HEAD
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
-=======
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
->>>>>>> db0443a396991cf5b2fd4e765b866fc5d8077bca
 
           <div
             className="hidden w-full md:block md:w-auto"
@@ -126,7 +96,6 @@ const Navbar = () => {
                 </div>
               ) : (
                 <>
-<<<<<<< HEAD
                 <li>
                   <NavLink
                     to="/"
@@ -165,46 +134,6 @@ const Navbar = () => {
     
             
               </>
-=======
-                  <li>
-                    <NavLink
-                      to="/"
-                      className="block py-2 pl-3 pr-4 text-gray-50 rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-
-                  <li>
-                    <NavLink
-                      to="/productos"
-                      className="block px-4 py-2 hover:bg-blue-600 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Productos
-                    </NavLink>
-                  </li>
-
-                  <li>
-                    <div className="nav__icons">
-                      <NavLink
-                        className="block py-2 pl-3 pr-4 text-gray-50 rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        to={"/cart"}
-                      >
-                        Carrito
-                        <p className="">${formatPrice(total)}</p>
-                      </NavLink>
-                    </div>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/login"
-                      className="block py-2 pl-3 pr-4 text-gray-50 rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Login
-                    </NavLink>
-                  </li>
-                </>
->>>>>>> db0443a396991cf5b2fd4e765b866fc5d8077bca
               )}
             </ul>
           </div>
