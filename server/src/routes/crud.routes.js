154f63cdd1458e00764f554a182486d8b3c14196
plopezoliva <schrammlopez@gmail.com>
2023-05-12 17:44:32 -0400

    se agrega crud.routes

--- server/src/routes/crud.routes.js
+++ server/src/routes/crud.routes.js
@@ -1,26 +1,26 @@
 const {Router} = require('express');
 const pool = require('../database/db');
 const router = Router();
-const {   getPrueba, crearProducto } = require('../controllers/crud.controllers')
+const {   getPrueba, crearProducto,getTodosLosProductos, getUnProducto, modificarProducto, eliminarProducto } = require('../controllers/crud.controllers')
 
 
 
 
-router.get('/prueba',getPrueba )
+router.get('/productos',getPrueba )
 
 //muestra todos los productos
-// router.get('/productos',getTodosLosProductos )
+router.get('/productos', getTodosLosProductos )
 
 //muestra un producto
-// router.get('/productos',getUnProducto)
+router.get('/productos/:id',getUnProducto)
 
 //crea un producto
 router.post('/productos', crearProducto)
 
 //modifica un producto
-// router.put('/productos/:id', modificarProducto)
+router.put('/productos/:id', modificarProducto)
 
 //elimina un producto
-// router.delete('/productos/:id', eliminarProducto)
+router.delete('/productos/:id', eliminarProducto)
 
 module.exports = router;
